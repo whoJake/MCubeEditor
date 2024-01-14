01127159609e1d51684cbf15160dd92d1c245709
Jake Cooper <37589250+whoJake@users.noreply.github.com>
2024-01-14 23:45:33 +0000

    Add QOL macros to jclog

--- src/tools/Framework/Base/logging/jclog.h
+++ src/tools/Framework/Base/logging/jclog.h
@@ -3,3 +3,13 @@
 #include "Log.h"
 #include "ConsoleTarget.h"
 #include "FileTarget.h"
+
+#define JCLOG_NONE(logref, fmt, ...) (logref).none(__FUNCTION__, fmt, __VA_ARGS__)
+#define JCLOG_TRACE(logref, fmt, ...) (logref).trace(__FUNCTION__, fmt, __VA_ARGS__)
+#define JCLOG_DEBUG(logref, fmt, ...) (logref).debug(__FUNCTION__, fmt, __VA_ARGS__)
+#define JCLOG_PROFILE(logref, fmt, ...) (logref).profile(__FUNCTION__, fmt, __VA_ARGS__)
+#define JCLOG_INFO(logref, fmt, ...) (logref).info(__FUNCTION__, fmt, __VA_ARGS__)
+#define JCLOG_WARN(logref, fmt, ...) (logref).warn(__FUNCTION__, fmt, __VA_ARGS__)
+#define JCLOG_ERROR(logref, fmt, ...) (logref).error(__FUNCTION__, fmt, __VA_ARGS__)
+#define JCLOG_EVENT(logref, fmt, ...) (logref).event(__FUNCTION__, fmt, __VA_ARGS__)
+#define JCLOG_LOG(logref, level, fmt, ...) (logref).log(level, __FUNCTION__, fmt, __VA_ARGS__)
