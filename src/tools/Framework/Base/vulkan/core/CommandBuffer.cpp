879c3bda272a999b246a0e3ebf900e0011210fdd
Jake Cooper <37589250+whoJake@users.noreply.github.com>
2024-02-11 23:31:30 +0000

    Add basic methods for command buffers

--- src/tools/Framework/Base/vulkan/core/CommandBuffer.cpp
+++ src/tools/Framework/Base/vulkan/core/CommandBuffer.cpp
@@ -1,6 +1,11 @@
 #include "CommandBuffer.h"
+
 #include "CommandPool.h"
 #include "Device.h"
+#include "RenderPass.h"
+#include "vulkan/rendering/RenderTarget.h"
+#include "Framebuffer.h"
+#include "PipelineState.h"
 
 namespace vk
 {
@@ -39,6 +44,57 @@ VkResult CommandBuffer::reset(ResetMode mode)
 VkCommandBufferLevel CommandBuffer::get_level() const
 {
     return m_level;
+}
+
+VkResult CommandBuffer::begin(VkCommandBufferUsageFlags usage, const RenderPass* renderPass, const Framebuffer* framebuffer, uint32_t subpassIndex)
+{
+    m_state.reset();
+    m_descriptorSetLayoutBindingState.clear();
+
+    VkCommandBufferBeginInfo beginInfo{ VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO };
+    beginInfo.flags = usage;
+    
+    // TODO: Handle secondary buffer here
+
+    return vkBeginCommandBuffer(get_handle(), &beginInfo);
+}
+
+VkResult CommandBuffer::end()
+{
+    return vkEndCommandBuffer(get_handle());
+}
+
+void CommandBuffer::begin_render_pass(const RenderTarget*              renderTarget,
+                                      const RenderPass&                renderPass,
+                                      const Framebuffer&               framebuffer,
+                                      const std::vector<VkClearValue>& clearValues,
+                                      VkSubpassContents                contents)
+{
+    m_currentRenderPass.renderPass = &renderPass;
+    m_currentRenderPass.framebuffer = &framebuffer;
+
+    VkRenderPassBeginInfo beginInfo{ VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO };
+    beginInfo.renderPass = renderPass.get_handle();
+    beginInfo.framebuffer = framebuffer.get_handle();
+    beginInfo.renderArea.extent = framebuffer.get_extent();
+    beginInfo.clearValueCount = to_u32(clearValues.size());
+    beginInfo.pClearValues = clearValues.data();
+
+    vkCmdBeginRenderPass(get_handle(), &beginInfo, contents);
+
+    ColorBlendState blendState = m_state.get_color_blend_state();
+    blendState.attachments.resize(m_currentRenderPass.renderPass->get_color_output_count(m_state.get_subpass_index()));
+    m_state.set_color_blend_state(blendState);
+}
+
+void CommandBuffer::end_render_pass()
+{
+    vkCmdEndRenderPass(get_handle());
+}
+
+void CommandBuffer::bind_pipeline_layout(PipelineLayout& layout)
+{
+    m_state.set_pipeline_layout(layout);
 }
 
 } // vk
--- src/tools/Framework/Base/vulkan/core/CommandBuffer.h
+++ src/tools/Framework/Base/vulkan/core/CommandBuffer.h
@@ -8,6 +8,11 @@ namespace vk
 
 class Device;
 class CommandPool;
+class RenderTarget;
+class RenderPass;
+class Framebuffer;
+class PipelineState;
+class PipelineLayout;
 
 class CommandBuffer : public Resource<VkCommandBuffer>
 {
@@ -19,16 +24,39 @@ public:
         AlwaysAllocate,
     };
 
+    struct RenderPassBinding
+    {
+        const RenderPass* renderPass;
+        const Framebuffer* framebuffer;
+    };
+
     CommandBuffer(CommandPool& commandPool, VkCommandBufferLevel level);
     ~CommandBuffer();
 
     VkResult reset(ResetMode resetMode);
 
     VkCommandBufferLevel get_level() const;
+
+    VkResult begin(VkCommandBufferUsageFlags usage, const RenderPass* renderPass, const Framebuffer* framebuffer, uint32_t subpassIndex);
+
+    VkResult end();
+
+    void begin_render_pass(const RenderTarget*              renderTarget,
+                           const RenderPass&                renderPass,
+                           const Framebuffer&               framebuffer,
+                           const std::vector<VkClearValue>& clearValues,
+                           VkSubpassContents                contents = VK_SUBPASS_CONTENTS_INLINE);
+
+    void end_render_pass();
+
+    void bind_pipeline_layout(PipelineLayout& layout);
 private:
     CommandPool& m_commandPool;
-
     VkCommandBufferLevel m_level;
+
+    RenderPassBinding m_currentRenderPass;
+    PipelineState m_state;
+    std::unordered_map<uint32_t, DescriptorSetLayout*> m_descriptorSetLayoutBindingState;
 };
 
 } // vk
